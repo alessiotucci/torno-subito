306da6c5df96ca81a6d87d14d8d4f8f89efbb103
Alessio Tucci <alessiotucci33@gmail.com>
2023-07-22 11:04:41 +0200

    esercizio di sabato

---
+++ esercizioFRahCONsole.cs
@@ -0,0 +1,43 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("---The  program start  here!---");
+
+// we create a async  function
+
+async Task GetDataAsync(int time, int id)
+{
+    // opeazione asincrona;
+    await Task.Delay(time);
+    Console.WriteLine($"dati: {id}");
+}
+
+Console.WriteLine("ora si inizia!");
+await GetDataAsync(9000, 12);
+
+
+
+await GetDataAsync(900, 34);
+
+
+// this  is the thing
+Task esecuzione = Task.Run(() => GetDataAsync(100, 56));
+Task esecuzione2 = Task.Run(() => GetDataAsync(1, 78));
+
+//Console.ReadLine(); this was used  to wait for the task to be finished ...
+// instead of using the method console read line we can use
+// metodo when all and when any!
+
+
+await Task.WhenAll(esecuzione, esecuzione2);
+
+
+await Task.WhenAny(esecuzione, esecuzione2);
+
+
+
+Console.WriteLine("finished the exectuiton");
+
+
+
